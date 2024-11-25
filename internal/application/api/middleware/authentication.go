ccd20a0c4316fef60897285be780f3728b5b89e9
oprimogus <gustavo.jesus@proton.me>
2024-11-25 00:34:34 -0300

    Use zitadel and fix a lot of stuff

---
+++ internal/application/api/middleware/authentication.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"github.com/oprimogus/cardapiogo/internal/infrastructure/services/zitadel"
+	"github.com/zitadel/zitadel-go/v3/pkg/authorization"
+	"net/http"
+)
+
+func Authentication(next http.Handler) http.Handler {
+	return zitadel.GetInstance().Middleware.RequireAuthorization()(next)
+}
+
+func Authorization(authorities ...string) func(http.Handler) http.Handler {
+	zt := zitadel.GetInstance()
+
+	return func(next http.Handler) http.Handler {
+		if len(authorities) == 0 {
+			panic("Authorization middleware requires at least one authority")
+		}
+
+		options := make([]authorization.CheckOption, len(authorities))
+		for i, authority := range authorities {
+			options[i] = authorization.WithRole(authority)
+		}
+
+		return zt.Middleware.RequireAuthorization(options...)(next)
+	}
+}
